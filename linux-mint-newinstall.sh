93fd6751304e50b1fdeda81313245fe80a1dd1c3
sullyjman <jaysinps@gmail.com>
2018-05-29 17:50:45 -0500

    Update linux-mint-newinstall.sh
    removed atom
    
    Added chrome download and install commands
    
    Added MS Visual Studio Code wget

--- linux-mint-newinstall.sh
+++ linux-mint-newinstall.sh
@@ -1,14 +1,20 @@
-
-#atom repo
-sudo add-apt-repository ppa:webupd8team/atom
-
 #update repo
 sudo apt-get update
 
-#to install software, need to get chrome 
+#to install software
 apt-get -y install dropbox vim atom python3 python3-pip idle3 numlockx mint-backgrounds-*
 
+#Chrome download
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+#install chrome
+sudo dpkg -i google-chrome*.deb
+
 #command to add chrome and atom to the panel
+
+#download MS Visual Studio Code
+
+wget https://go.microsoft.com/fwlink/?LinkID=760868
 
 #removing all third party apps bs
 sudo apt-get -y --purge autoremove xpad* gimp* pix* simple-scan* thunderbird* transmission-gtk* brasero* xplayer* rhythmbox* firefox* hexchat* pidgin*
